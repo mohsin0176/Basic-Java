227546b1abfdd1a3927990a2178399044cfade24
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-10 10:46:56 -0700

    typecast01 java programme

---
+++ typecasting/typecast1.java
@@ -0,0 +1,15 @@
+public class Main
+{
+
+public static void main(String[] args) 
+	{
+
+		int m=9;
+		double myDouble=(double) m;
+
+		System.out.println(m);
+		System.out.println(myDouble);
+		
+	}
+
+}
