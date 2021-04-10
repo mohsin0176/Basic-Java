c7c72431b0825df32064d01af3bd2e78de2e5121
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-10 10:44:51 -0700

    typecast java programme

---
+++ typecasting/typecast.java
@@ -0,0 +1,15 @@
+public class Main
+{
+
+public static void main(String[] args) 
+	{
+
+		double myDouble=9.78;
+		int myInt=(int) myDouble;
+
+		System.out.println(myDouble);
+		System.out.println(myInt);
+		
+	}
+
+}
