45c2bfbf95693ca9a9b3eefee263fb0e3bb28fd1
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-10 11:35:12 -0700

    String Conacte java programme

---
+++ JavaString/string6.java
@@ -0,0 +1,12 @@
+public class main
+{
+	public static void main(String[] args)
+	 {
+            
+            String firstname="John";
+            String lastname="Doe";
+            System.out.println(firstname.concat(lastname));
+
+	 }
+	
+}
