180412c223cceb8b28e764c1895a4bdc7b34ab7e
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-11 07:13:08 -0700

    Java date

---
+++ JavaDate/date.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate; // import the LocalTime class
+
+public class Main {
+  public static void main(String[] args) {
+    LocalTime myObj = LocalDate.now();
+    System.out.println(myObj);
+  }
+}
