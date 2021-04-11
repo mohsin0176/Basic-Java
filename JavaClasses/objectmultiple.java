50e66727d67e6fb04923279f63cb60f47ae1b13c
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-11 05:57:44 -0700

    Java Create multiple Object

---
+++ JavaClasses/objectmultiple.java
@@ -0,0 +1,11 @@
+public class main 
+{
+	int x=12;
+	public static void main(String[] args)
+	{
+		Main obj=new Main();
+		Main object=new Main();
+		System.out.println(obj.x);
+		System.out.println(object.x);
+	}
+}
