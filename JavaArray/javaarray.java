4e7ec5dfae595a22b1ed858b54f7818a7209c7d3
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-11 00:17:37 -0700

    Java array first

---
+++ JavaArray/javaarray.java
@@ -0,0 +1,12 @@
+public class main 
+{
+ public static void main(String[] args)
+  {
+
+String[] cars={"volvo","bmw","mazda"};
+for (i=0;i<=cars.length ;i++ ) 
+{
+	System.out.println(cars[i]);		
+}		
+  }
+}
