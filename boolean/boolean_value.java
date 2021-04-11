7673b543a208b62a8dc988d50970ac483173a936
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-10 23:27:37 -0700

    boolean value

---
+++ boolean/boolean_value.java
@@ -0,0 +1,10 @@
+ public class main
+{
+public static void main(String[] args)
+{
+boolean isJavaFun=true;	
+boolean isJavaTasty=false;	
+System.out.println(isJavaTasty);
+System.out.println(isJavaFun);
+}
+}
