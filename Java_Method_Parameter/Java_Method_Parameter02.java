dbed24503f6a2c8360b6d7f661d1f7687e7c35cd
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-11 04:52:00 -0700

    Java method with two  Parameter

---
+++ Java_Method_Parameter/Java_Method_Parameter02.java
@@ -0,0 +1,11 @@
+public class Main {
+  static void myMethod(String fname,int age) {
+    System.out.println(fname + " Refsnes"+age);
+  }
+
+  public static void main(String[] args) {
+    myMethod("Liam",15);
+    myMethod("Jenny",15);
+    myMethod("Anja",15);
+  }
+}
