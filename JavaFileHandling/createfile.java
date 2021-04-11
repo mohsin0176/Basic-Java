54ecd7461f8699ebf2236c1181de7096d537bc2a
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-11 09:46:50 -0700

    Create A File in Java

--- JavaFileHandling/createfile.java
+++ JavaFileHandling/createfile.java
@@ -4,7 +4,7 @@ import java.io.IOException;  // Import the IOException class to handle errors
 public class CreateFile {
   public static void main(String[] args) {
     try {
-      File myObj = new File("filename.txt");
+      File myObj = new File("C:\\Users\\MyName\\filename.txt");  
       if (myObj.createNewFile()) {
         System.out.println("File created: " + myObj.getName());
       } else {
